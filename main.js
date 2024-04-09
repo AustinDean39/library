ed1e6f3e1090e2917038d63f19e754ff729043d4
Austin Dean <19595957+AustinDean39@users.noreply.github.com>
2024-04-09 15:19:12 -0400

    Create displayLibrary function

--- main.js
+++ main.js
@@ -13,4 +13,28 @@ function Book(title, author, pages, readStatus) {
 
 function addBookToLibrary(book) {
   myLibrary.push(book);
+  displayLibrary();
+}
+
+function displayLibrary() {
+  document.getElementById('shelf').innerHTML = '';
+  
+  for (const book of myLibrary) {
+    //Create card for book display
+    const bookCard = document.createElement('div');
+    const bookTitleHeading = document.createElement('h2');
+    const bookAuthorPara = document.createElement('p');
+    const bookPagesPara = document.createElement('p');
+    const bookReadStatusPara = document.createElement('p');
+    
+    //Add in info
+    bookTitleHeading.textContent = book.title;
+    bookAuthorPara.textContent = book.author;
+    bookPagesPara.textContent = book.pages + ' pages';
+    bookReadStatusPara.textContent = book.readStatus ? 'read' : 'not read yet';
+    
+    // Append everything to everything
+    bookCard.append(bookTitleHeading, bookAuthorPara, bookPagesPara, bookReadStatusPara);
+    document.getElementById('shelf').appendChild(bookCard);
+  }
 }
